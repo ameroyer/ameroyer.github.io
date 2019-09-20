a1544826788dd4b2221f2f2f0542bd9e0c4ea3a6
Amelie Royer <amelie.royer@ist.ac.at>
2019-09-20 11:15:45 +0200

    update

--- notebooks/2019_03_01_canteen_analysis/idyll-sources/components/weighted-word-list.js
+++ notebooks/2019_03_01_canteen_analysis/idyll-sources/components/weighted-word-list.js
@@ -3,44 +3,47 @@ const D3Component = require('idyll-d3-component');
 const d3 = require('d3');
 
 const transition_duration = 625;
+const padding = 30;
+const base_font_size = 8;
+const width = 200;
+const height = 200;
+
+function load(svg, data) {
+    let hstep = (height - padding) / data.length;
+    
+    svg.selectAll("text")
+	.data(data)
+	.enter()
+	.append("text") 
+	.attr("text-anchor", "middle")
+	.attr("x", width / 2)
+	.attr("y", function(d) { return d.index * hstep;})
+	.attr("dy", padding)
+	.style("font-size", function(d){return base_font_size + d.count })
+	.text(function(d) {return d.name;})
+	.style("fill", "white") 
+	.style("opacity", 0.)
+	.transition()
+	.duration(transition_duration)
+	.delay(function(d) {return d.index * transition_duration * 0.3;})
+	.style("opacity", 1.);
+}
 
 class WeightedWordList extends D3Component {
 
 
-    initialize(node, props) {	
-	var width = 960;
-	var height = 400;
-
-	var svg = (this.svg = d3.select(node).append('svg'));
-	//svg.append("rect")
-	 //   .attr("width", "100%")
-	  //  .attr("height", "100%");
-	
+    initialize(node, props) {
+	var svg = (this.svg = d3.select(node).append('svg'));	
 	svg
 	    .attr('viewBox', `0 0 ${width} ${height}`)
-	    .style('width', '${width}')
-	    .style('height', '${height}');
-	
-	svg.selectAll("text")
-	    .data(props.data)
-	    .enter()
-	    .append("text") // append text
-	    .attr("x", 200)
-	    .attr("y", function(d) { return d.index * 40;})
-	    .attr("dy", 40)
-	    .attr("text-anchor", "middle") // set anchor y justification
-	    .style("font-size", function(d){return 12 + d.count * 3})
-	    .text(function(d) {return d.name;})
-	    .style("fill", "white") // make the body green
-	    .transition()
-	    .duration(transition_duration)
-	    .delay(function(d) {return d.index * transition_duration * 0.3;})
-	    .style("fill", "red"); // define the text to display	
+	    .style('width', '100%')
+	    .style('height', '100%');	
     }
 
     update(props, oldProps) {
+	this.svg.selectAll("text").remove();
 	if (props.load) {
-	    this.loadData(props);
+	    load(this.svg, props.data);
 	}
     }
 }
